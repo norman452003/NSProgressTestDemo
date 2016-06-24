5723113dfe57f83740ea85f9c43f3a03e0eb09cc
norman <87526845@qq.com>
2016-06-24 15:30:14 +0800

    Initial Commit

---
+++ NSProgressDemo2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSProgressDemo2
+//
+//  Created by gongxin on 16/6/24.
+//  Copyright © 2016年 gongxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NSProgressDemo2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  NSProgressDemo2
+//
+//  Created by gongxin on 16/6/24.
+//  Copyright © 2016年 gongxin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
